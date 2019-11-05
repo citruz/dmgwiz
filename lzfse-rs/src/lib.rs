d7e693b81a5b030fc4ac33342a069f33dc4ad8d2
Felix Seele <fseele@gmail.com>
2019-11-05 20:39:56 +0100

    added basic tests to lzfse-rs

--- lzfse-rs/src/lib.rs
+++ lzfse-rs/src/lib.rs
@@ -6,6 +6,25 @@ use libc::size_t;
 #[derive(PartialEq, Debug)]
 pub enum Error {
     BufferTooSmall,
+    CompressFailed,
+}
+
+pub fn encode_buffer(input: &[u8], output: &mut [u8]) -> Result<usize, Error> {
+    let out_size = unsafe {
+        ffi::lzfse_encode_buffer(
+            output.as_ptr() as *mut _,
+            output.len() as size_t,
+            input.as_ptr() as *const _,
+            input.len() as size_t,
+            0 as *mut _,
+        ) as usize
+    };
+
+    if out_size == 0 {
+        return Err(Error::CompressFailed);
+    } else {
+        return Ok(out_size);
+    }
 }
 
 pub fn decode_buffer(input: &[u8], output: &mut [u8]) -> Result<usize, Error> {
@@ -23,5 +42,60 @@ pub fn decode_buffer(input: &[u8], output: &mut [u8]) -> Result<usize, Error> {
         return Err(Error::BufferTooSmall);
     } else {
         return Ok(out_size);
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    extern crate rand;
+
+    use super::*;
+
+    #[test]
+    fn round_trip() {
+        let input:Vec<u8> = (0..1024).map(|_| { rand::random::<u8>() }).collect();
+        // lzfse will fallback to return the input uncompressed and add a magic header to indicate this
+        // this requires 12 byte (see lzfse_encode.c)
+        let max_outlen = input.len() + 12;
+        let mut compressed = vec![0; max_outlen];
+
+        let bytes_out = encode_buffer(&input[..], &mut compressed[..]).unwrap();
+        assert_ne!(bytes_out, 0);
+    
+        // need to allocate 1 byte more since lzfse returns input.len() if the buffer is too small 🤷‍♂️
+        let mut uncompressed = vec![0; input.len() + 1];
+        let bytes_in = decode_buffer(&compressed[0..bytes_out], &mut uncompressed[..]).unwrap();
+
+        assert_eq!(bytes_in, input.len());
+        assert_eq!(input[..], uncompressed[..bytes_in]);
+    }
+
+    #[test]
+    fn decode_buffer_to_small() {
+        let input:Vec<u8> = (0..1024).map(|_| { rand::random::<u8>() }).collect();
+        // lzfse will fallback to return the input uncompressed and add a magic header to indicate this
+        // this requires 12 byte (see lzfse_encode.c)
+        let max_outlen = input.len() + 12;
+        let mut compressed = vec![0; max_outlen];
+
+        let bytes_out = encode_buffer(&input[..], &mut compressed[..]).unwrap();
+        assert_ne!(bytes_out, 0);
+    
+        // this is one byte too small
+        let mut uncompressed = vec![0; input.len()];
+        let result = decode_buffer(&compressed[0..bytes_out], &mut uncompressed[..]);
+
+        assert_eq!(result, Err(Error::BufferTooSmall));
+    }
+
+    #[test]
+    fn encode_buffer_to_small() {
+        let input = [0xC0, 0xFF, 0xEE, 0xBA, 0xBE];
+        let max_outlen = input.len();
+        let mut compressed = vec![0; max_outlen];
+
+        // this is not compressible
+        let result = encode_buffer(&input[..], &mut compressed[..]);
+        assert_eq!(result, Err(Error::CompressFailed));
     }
 }
